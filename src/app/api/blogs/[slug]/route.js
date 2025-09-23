41b572d414924af8d64fdf679adec0921f47b5b3
Aditya Kumar <webitya@gmail.com>
2025-09-23 23:17:18 +0530

    Aditya

--- src/app/api/blogs/[slug]/route.js
+++ src/app/api/blogs/[slug]/route.js
@@ -1,5 +1,5 @@
 import { NextResponse } from "next/server";
-import dbConnect from "../../../lib/mongodb";
+import dbConnect from "../../../../lib/mongodb";
 import Blog from "../../../../models/Blog";
 import { isAdminRequest } from "../../../../lib/admin-auth";
 
